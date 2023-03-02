a2f0d15fb08155ee281a346486d61aa5ceac897c
Zach Ruiz <powersbooth10@gmail.com>
2023-03-02 13:10:30 -0600

    Setup the css and shuffle logic for  memory game

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,38 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
+const cardImages = [
+  {"src": "img/blackHole1.png"},
+  {"src": "img/blackHole2.png"},
+  {"src": "img/blackHole3.png"},
+  {"src": "img/bloodSplatter.png"},
+  {"src": "img/explodingStar.png"},
+]
+
+
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+
+
+  //shuffle cards
+  const shuffleCards = ()  => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card =>({...card, id: Math.random()}))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
 
   return (
     <div className="App">
-      <h1>Page Count: {count}</h1>
+      <h1>MemeMatch</h1>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
